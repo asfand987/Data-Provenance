14a96c73896f1dc9cc7ab21981f966c6e4a18764
asfand987 <asfand987.ak@gmail.com>
2021-02-18 11:55:25 +0000

    Intergrated Spring boot and Maven.

---
+++ src/main/java/com/data/provenance/DataProvenance.java
@@ -0,0 +1,11 @@
+package com.data.provenance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataProvenance {
+    public static void main(String[] args) {
+        SpringApplication.run(DataProvenance.class, args);
+    }
+}
---
+++ src/main/java/com/data/provenance/controller/ProvController.java
@@ -0,0 +1,12 @@
+package com.data.provenance.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ProvController  {
+    @RequestMapping("/")
+    public String start() {
+        return "index";
+    }
+}

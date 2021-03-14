9b6028443e92113eb4072272d72169d65a98a958
asfand987 <asfand987.ak@gmail.com>
2021-03-14 22:12:08 +0000

    Added RequestMapping POST method to recieve JSON in server

--- src/main/java/com/data/provenance/controller/ProvController.java
+++ src/main/java/com/data/provenance/controller/ProvController.java
@@ -13,4 +13,10 @@ public class ProvController  {
         System.out.println("YES-----------------------------");
         return "index";
     }
+
+    @RequestMapping(value = "/", method = RequestMethod.POST)//, consumes = "text/plain")
+    @ResponseBody
+    public void PROV_JSON(@RequestBody String json) throws Exception {
+        System.out.println(json);
+    }
 }

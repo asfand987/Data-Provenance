aaa5120fadf2b04936723d3598749a535052f415
asfand987 <asfand987.ak@gmail.com>
2021-03-15 15:31:38 +0000

    Namespace HashMap is populated

--- src/main/java/com/data/provenance/controller/ProvController.java
+++ src/main/java/com/data/provenance/controller/ProvController.java
@@ -1,23 +1,57 @@
 package com.data.provenance.controller;
 
+import org.json.JSONArray;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 @Controller
-public class ProvController  {
+public class ProvController {
+
+    private HashMap<String, String> namespaceMap = new HashMap<>();
+
     @RequestMapping("/")
     public String start() {
-        System.out.println("YES-----------------------------");
         return "index";
     }
 
     @RequestMapping(value = "/", method = RequestMethod.POST)//, consumes = "text/plain")
     @ResponseBody
     public void PROV_JSON(@RequestBody String jsonString) throws Exception {
-        System.out.println(jsonString);
-        
+        JSONArray jsonArray = new JSONArray(jsonString);
+        populateNamespaceMap(jsonArray);
     }
+
+
+    public void populateNamespaceMap(JSONArray jsonArray) {
+        try {
+            JSONObject namespaceObject = new JSONObject(jsonArray.get(0).toString());
+            JSONArray namespaceJSONArr = new JSONArray(namespaceObject.get("prefix").toString());
+
+            for (int i = 0; i < namespaceJSONArr.length(); i++) {
+                JSONObject json = namespaceJSONArr.getJSONObject(i);
+                Iterator<String> keys = json.keys();
+                while (keys.hasNext()) {
+                    String key = keys.next();
+                    //System.out.println("Key :" + key + "  Value :" + json.get(key));
+                    namespaceMap.put(key, json.get(key).toString());
+                }
+            }
+        }
+        catch (Exception e) {
+        }
+//        for (Map.Entry<String, String> entry : namespaceMap.entrySet()) {
+//            System.out.println(entry.getKey()+" : "+entry.getValue());
+//        }
+    }
+
+
 }
+

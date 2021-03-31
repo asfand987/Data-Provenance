1f6aa816b1edd5f3ffeb5607ea20684e52487a7e
asfand987 <asfand987.ak@gmail.com>
2021-03-31 14:16:15 +0100

    Refactored Inspector js files

--- src/main/resources/static/js/components/inspector/createAttrSection.js
+++ src/main/resources/static/js/components/inspector/createAttrSection.js
@@ -1,18 +1,26 @@
     
+/**
+ * This file contains code for creating new attributes in the Inspector component.
+ * 
+ */
+
+//-------------------------------------------------------------------------------------------------/
+
 /*
-**  This function is responsible for letting the user enter in data to elements, this is done inside the inspector.
-**  
+*  This function is responsible for letting the user enter in data to elements, 
+*  this is done inside the inspector.
+*  
 */
 document.getElementById("chooseAttributeBtn").addEventListener("click",  function() {
-    userEnterDataIntoElement(clickedElementID);
+    createNewAttributes(clickedElementID);
 });  
 
-function userEnterDataIntoElement(id) {
+function createNewAttributes(id) {
     let elementDataDivParent = document.getElementById("inspectorAttrContainer");
     let elementDataDiv = document.getElementById(id+"-attributesContainerDivParent");
     let attributeSelectionDiv =  document.getElementById("selectionAttributes");
     
-    //------------create new form and corresponding elements for user to enter in new data and save--------------//
+    //create new form and corresponding elements for user to enter in new data and save.
     let form = document.createElement('form');
 
     //input
@@ -23,13 +31,11 @@ function userEnterDataIntoElement(id) {
 
     //button
     let attrSaveBtn = document.createElement("button");
-    //-----------------------------------------------------------------------------------------------------------//
-    //-----------------------------------------------------------------------------------------------------------//
 
     
     let userSelectedAttrNamespace = attributeSelectionDiv.options[attributeSelectionDiv.selectedIndex].text;
     
-    //-------------------------------------------------------Alert prompt----------------------------------------//
+    //Alert prompt
     if(attributeSelectionDiv.options[attributeSelectionDiv.selectedIndex].parentElement.label == "Attributes") {
         let alertPromptToEnterAttrType = prompt("Enter " +  userSelectedAttrNamespace + " type", );
         if(!alertPromptToEnterAttrType)   return;
@@ -38,33 +44,27 @@ function userEnterDataIntoElement(id) {
     else {
         attrLabel.innerHTML = userSelectedAttrNamespace;
     }
-    //-----------------------------------------------------------------------------------------------------------//
-    //-----------------------------------------------------------------------------------------------------------//
     
-
-    //-----------------------------------------------------Input-------------------------------------------------//
+    //Input info
     userInputAttr.id = id+"-Input";
     userInputAttr.type = 'text';
     userInputAttr.name = 'name';
 
-    //-----------------------------------------------------Button-------------------------------------------------//
+    //Button info
     attrSaveBtn.innerHTML = "✓";
     attrSaveBtn.className = "btn btn-warning";//"inspectorBtn"
     attrSaveBtn.type = 'button';
     attrSaveBtn.id = id+"-Button";
 
-    //-----------------------------------------------------Label--------------------------------------------------//
+    //Label info
     attrLabel.id = id+"-Label";
 
-    //-----------------------------------------------------Form---------------------------------------------------//
     $(form).submit(function (e) {
         e.preventDefault();
     });
     form.appendChild(attrLabel);
     form.appendChild(userInputAttr);
     form.appendChild(attrSaveBtn);
-    //------------------------------------------------------------------------------------------------------------//
-    //------------------------------------------------------------------------------------------------------------//
 
     /*
     **  if elementDataDiv (div where data is held) already exists, add the form (data) to it and then add to parentDiv.
@@ -81,79 +81,8 @@ function userEnterDataIntoElement(id) {
         elementDataDivParent.appendChild(elementDataDiv);
     }
     
-    addDataToElement(id, userInputAttr, attrLabel, attrSaveBtn, form);
+    displayAttrInInspector(id, userInputAttr, attrLabel, attrSaveBtn, form);
     
 }
 
 
-/*
-** This function checks if dataDiv already exists, if so, add all existing data to it to display.
-** Otherwise create a new one.
-** Used in @userEnterDataIntoElement(id).
-*/
-function addDataToElement(id, input, label, button, form) { 
-    let dataDiv = document.getElementById(id+"-attributesContainerDiv");
-    
-    if(!dataDiv) {
-        dataDiv = document.createElement('div');
-        dataDiv.id = id+"-attributesContainerDiv";
-    }
-
-    //------------------------------------------------------------------------------------------------------------//
-
-    /*
-    **  Display user entered in data to the inspector. Deletes the form used to enter in the data.
-    **  Called when '+' button is pressed in the inspector.
-    ** Returns dataDiv.
-    */
-    showNewDataInInspector(id, input, label, button, dataDiv);
-
-    form.appendChild(dataDiv);
-}
-
-
-/*
-**  Add data which user has entered from form into Inspector to display
-**
-*/
-function showNewDataInInspector(id, input, label, button, dataDiv) {
-    document.getElementById(id+"-Button").addEventListener("click", function() { 
-        let br = document.createElement("br");
-        let attrButton = document.createElement("button");
-        let attrlabel = document.createElement("label");
-        let attributeValue = document.getElementById(id+"-Input").value;
-        let element = document.getElementById(id);
-
-        attrButton.className = "btn btn-danger"
-
-        //------------------------------------Add data directly onto element-----------------------------------------//
-        element.attributes[2].nodeValue =  element.attributes[2].nodeValue + label.innerHTML+ ": " + attributeValue + ",";
-        
-        //-----------------------------------------------------------------------------------------------------------//
-
-        //----------------------------------Get all data stored In element-------------------------------------------//
-        let elementAttributes = element.attributes[2].nodeValue.split(",");
-        elementAttributes.pop();
-        //-----------------------------------------------------------------------------------------------------------//
-
-        //----------------------------------Display all data stored in element in Inspector--------------------------//
-        for (let i = 0; i < elementAttributes.length; i++) {
-            attrButton.innerHTML = "X";
-            attrButton.type = 'button';
-            attrButton.id = elementAttributes[i];
-
-            //-------------------------------Delete data on from Inspector on click----------------------------------//
-            attrButton.addEventListener("click", function() { deleteData(attrlabel, attrButton, br, element); });
-            //-------------------------------------------------------------------------------------------------------//
-
-            attrlabel.innerHTML = elementAttributes[i];
-            dataDiv.appendChild(attrlabel);
-            dataDiv.appendChild(attrButton);
-            dataDiv.appendChild(br);
-        }
-            //----------------------------------Remove From used to enter in new attribute data----------------------//
-            removeForm(input, label, button);
-        });
-
-        return dataDiv;
-}
--- src/main/resources/static/js/components/inspector/displayAttrSection.js
+++ src/main/resources/static/js/components/inspector/displayAttrSection.js
@@ -1,11 +1,76 @@
 
+/*
+** This function checks if container div already exists, if so, add all existing data to it to display.
+** Otherwise create a new one.
+** Used in @createNewAttributes(id).
+*/
+function displayAttrInInspector(id, input, label, button, form) { 
+    let attributeDiv = document.getElementById(id+"-attributesContainerDiv");
+    
+    if(!attributeDiv) {
+        attributeDiv = document.createElement('div');
+        attributeDiv.id = id+"-attributesContainerDiv";
+    }
+
+    //   Display user entered in data to the inspector. Deletes the form used to enter in the data.
+    //   Called when '+' button is pressed in the inspector.
+    //   Returns attributeDiv.
+    
+    createComponentsToDisplay(id, input, label, button, attributeDiv);
+
+    form.appendChild(attributeDiv);
+}
+
+
+/*
+**  Create components for attribute which user has entered from form into Inspector to display
+**
+*/
+function createComponentsToDisplay(id, input, label, button, attributeDiv) {
+    document.getElementById(id+"-Button").addEventListener("click", function() { 
+        let br = document.createElement("br");
+        let attrButton = document.createElement("button");
+        let attrLabel = document.createElement("label");
+        let attributeValue = document.getElementById(id+"-Input").value;
+        let element = document.getElementById(id);
+
+        attrButton.className = "btn btn-danger"
+
+        //Add data directly onto DOM object element
+        element.attributes[2].nodeValue =  element.attributes[2].nodeValue + label.innerHTML+ ": " + attributeValue + ",";
+        
+
+        //Get all data stored in DOM element
+        let elementAttributes = element.attributes[2].nodeValue.split(",");
+        elementAttributes.pop();
+
+        //Create components of all existing attributes belonging to element
+        for (let i = 0; i < elementAttributes.length; i++) {
+            attrButton.innerHTML = "X";
+            attrButton.type = 'button';
+            attrButton.id = elementAttributes[i];
+
+            //Delete attribute on from Inspector and remove value from DOM object on click
+            attrButton.addEventListener("click", function() { deleteAttribute(attrLabel, attrButton, br, element); });
+
+            attrLabel.innerHTML = elementAttributes[i];
+            attributeDiv.appendChild(attrLabel);
+            attributeDiv.appendChild(attrButton);
+            attributeDiv.appendChild(br);
+        }
+            //Remove From used to enter in new attribute data
+            removeForm(input, label, button);
+        });
+
+        return attributeDiv;
+}
 
 
 /*
 **  Each attribute has an "X" button to delete it. This function will remove remnants of that data from the Inspector.
 **
 */
-function deleteData(label, button, br, element) {
+function deleteAttribute(label, button, br, element) {
     label.remove();
     button.remove();
     br.remove();
@@ -21,18 +86,4 @@ function removeForm(input, label, button) {
     input.remove();
     label.remove(); 
     button.remove();
-}
-
-
-
-
-function alertBox(message, placeholder) { 
-    let value;
-    let promptAlert = prompt(message, placeholder);
-    if (promptAlert == null || promptAlert == "") {
-        value = "User cancelled the prompt.";
-    } else {
-        value =  promptAlert;
-    }
-    return value;
 }
